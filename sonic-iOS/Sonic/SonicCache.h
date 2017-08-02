253218b49585fe65dc9a0e01d8d87a5adbe0d689
zyvincenthu <hutao5262705@hotmail.com>
2017-08-02 20:31:48 +0800

    iOS修改版权声明

--- sonic-iOS/Sonic/SonicCache.h
+++ sonic-iOS/Sonic/SonicCache.h
@@ -6,14 +6,14 @@
 //  Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
 //  Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
 //  in compliance with the License. You may obtain a copy of the License at
+//
 //  https://opensource.org/licenses/BSD-3-Clause
+//
 //  Unless required by applicable law or agreed to in writing, software distributed under the
 //  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 //  either express or implied. See the License for the specific language governing permissions
 //  and limitations under the License.
 //
-//  Created by zyvincenthu on 17/4/1.
-//  Review  by ianhuang
 //  Copyright © 2017年 Tencent. All rights reserved.
 //
 
@@ -22,12 +22,12 @@
 #import "SonicConstants.h"
 
 /**
- * @abstract Manage the memory caches and file cache for all sonic sessions.
+ * @brief Manage the memory caches and file cache for all sonic sessions.
  */
 @interface SonicCache : NSObject
 
 /**
- * @abstract Return an cache manage instance,it can create or update memory cache and file.
+ * @brief Return an cache manage instance,it can create or update memory cache and file.
  * cache for each sonic session.
  */
 + (SonicCache *)shareCache;
@@ -89,7 +89,7 @@
 
 /**
  * @brief Check if there is memory cache or file cache.
- * @param sessionID:  an session id to find memory cache item.
+ * @param sessionID  an session id to find memory cache item.
  * @result If there is not exist an memory cache item and file cache data not exist
  * either, then return NO.
  
@@ -97,14 +97,14 @@
 - (BOOL)isFirstLoad:(NSString *)sessionID;
 
 /**
- * @brief Split: the HTML to dynamic data and template string.
- * @param HTML: the HTML document downloaded by sonic session.
+ * @brief Split the HTML to dynamic data and template string.
+ * @param html the HTML document downloaded by sonic session.
  */
 - (NSDictionary *)splitTemplateAndDataFromHtmlData:(NSString *)html;
 
 /**
  * @brief Merge the server data and local dynamic data to create new HTML and the difference data.
- * @abstract Step1. Get the difference between the "updateDict" and "existData",
+ * Step1. Get the difference between the "updateDict" and "existData",
            Step2. Update "existData" with the "updateDict",then merge with the "templateString",
            to build the new HTML cache.
  * @result Return the difference data and new HTML string.
@@ -113,7 +113,7 @@
 
 /**
  * @brief First time to save htmlData
- * @abstract First time we need split the HTML in two parts: template and dynamic data
+ * First time we need split the HTML in two parts: template and dynamic data
  * by the "<sonic-diff" tags.
  * @param htmlData the data download by sonic session,it is the whole HTML document.
  * @param headers the response headers from sonic session.
@@ -137,7 +137,7 @@
 
 /**
  * @brief   Get the memory cache item by sessionID.
- * @abstract If there no memory cache exist,then create an new item
+ * If there no memory cache exist,then create an new item
            If there has file cache exist,then read require data into memory.
  * @param sessionID  an session id to find memory cache item.
  * @result If cache is not exist then return nil.

a2dfa9883f4c1ac1865455a75fabb18a6713cc3c
vasdeveloper <30431775+vasdeveloper@users.noreply.github.com>
2017-08-11 15:54:46 +0800

    php demo兼容nginx
    php demo兼容nginx

--- sonic-php/sample/index.php
+++ sonic-php/sample/index.php
@@ -6,6 +6,21 @@
  * https://opensource.org/licenses/BSD-3-Clause
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
+if (!function_exists('getallheaders'))  {
+    function getallheaders()
+    {
+        if (!is_array($_SERVER)) {
+            return array();
+        }
+        $headers = array();
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
 require_once 'controller/demo.php';
 $demo = new controller_demo();
 $demo->actionIndex();
--- sonic-php/sample/util/sonic.php
+++ sonic-php/sample/util/sonic.php
@@ -12,6 +12,21 @@
  * @time 2017.03.24
  *
  */
+if (!function_exists('getallheaders'))  {
+    function getallheaders()
+    {
+        if (!is_array($_SERVER)) {
+            return array();
+        }
+        $headers = array();
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
 class TemplateReplace{
     public static $shotWnsDiffBodyReplace = false; //判断是否成功替换sonicdiffbody
     public static $diffIndex = 0; 

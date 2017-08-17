678b6a7c5ca1fb34edfc65c7021ac05e7fa48f57
Tong ZHAN <zhantong1994@163.com>
2017-08-17 11:22:10 +0800

    不再使用ObjectSerializer 使用简单的拼接实现List<String>的序列化和反序列化

--- sonic-android/sample/src/main/java/com/tencent/sonic/demo/MainActivity.java
+++ sonic-android/sample/src/main/java/com/tencent/sonic/demo/MainActivity.java
@@ -73,7 +73,6 @@ public class MainActivity extends Activity {
         btnDefault.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, DEMO_URL, Toast.LENGTH_LONG).show();
                 startBrowserActivity(MODE_DEFAULT);
             }
         });
